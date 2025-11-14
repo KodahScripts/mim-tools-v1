dd567e8b893e52582f9db709194c2c8dedee4f06
Matthew Beckwith <kodahscripts@outlook.com>
2025-11-14 08:55:07 -0500

    fix deploy links

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/mim-tools-v1/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
